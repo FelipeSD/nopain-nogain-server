910d392e83cb09c8fa9cb828584abfb9b608e577
Felipe Damasceno <felipe@superempresa.com.br>
2021-07-31 20:13:35 -0300

    small adjustments

--- app/controllers/client.controller.js
+++ app/controllers/client.controller.js
@@ -3,7 +3,7 @@ const db = require("../models");
 const Client = db.client;
 const TrainingSheet = db.trainingSheet;
 
-exports.create = (req, res) => {
+exports.create = async (req, res) => {
     let data = req.body;
 
     if(!data.name){
@@ -20,23 +20,22 @@ exports.create = (req, res) => {
         gender: data.gender
     });
 
-    client.save(clientModel).then(data=>{
+    try {
+        const data = await client.save(clientModel);
         res.send(data);
-    }).catch(err => {
+    }catch (e) {
         res.status(500).send({
-            message: err.message
-        })
-    })
+            message: e.message
+        });
+    }
 }
 
 exports.findOne = (req, res) => {
-    console.log(req.body);   
 
     Client.findOne(req.body).exec((err, client)=>{
-        console.log(client);
         if(!err){
             if(
-                client // 👈 null and undefined check
+                client
                 && Object.keys(client).length === 0 
                 && client.constructor === Object
             ){
@@ -48,71 +47,62 @@ exports.findOne = (req, res) => {
             res.status(500).send({ message: "Couldn't get client", err})
         }
     });
-    // .then(data => {
-    //     if(!data.length){
-    //         res.status(404).send({ message: "Client Not Found" });
-    //     }else{
-    //         res.send(data)
-    //     }
-    // }).catch(err => {
-    //     res.status(500).send({ message: "Couldn't get client"})
-    // });
 }
 
-exports.findAll = (req, res) => {
-    const condition = {...req.body};
-
-    Client.find(condition).then(data => {
-        res.send(data)
-    }).catch(err => {
+exports.findAll = async (req, res) => {
+    try {
+        const condition = {...req.body};
+        const data = await Client.find(condition)
+        res.send(data);
+    }catch (e) {
         res.status(500).send({ message: "Couldn't get client list"})
-    });
+    }
 }
 
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
     if(!req.body){
         res.status(400).send({ message: "Invalid data" });
         return;
     }
     
-    let id = req.params.id;
-    console.log(req.body);
-    Client.findByIdAndUpdate(id, req.body).then(data => {
+    try {
+        let id = req.params.id;
+        const data = await Client.findByIdAndUpdate(id, req.body);
         if (!data) {
             res.status(400).send({ message: "Can't update client" })
         } else {
             res.send({ message: "Client successfully updated" });
         }
-    }).catch(err => {
+    }catch (e) {
         res.status(500).send({ message: "Biiirll!!! O erro ta saindo da jaula" });
-    });
+    }
 }
 
-exports.delete = (req, res) => {
-    const id = req.params.id;
-
-    console.log(req.body, req.params);
-
-    Client.findByIdAndRemove(id).then(data => {
+exports.delete = async (req, res) => {
+    try{
+        const id = req.params.id;
+        const data = await Client.findByIdAndRemove(id);
         if (!data) {
             res.status(400).send({ message: "Can't remove client" })
         } else {
             TrainingSheet.deleteMany({owner: id}).exec();
             res.send({ message: "Client successfully removed" });
         }
-    }).catch(err => {
+    }catch (e) {
         res.status(500).send({ message: "Hoops! Error ocur" });
-    });
+    }
 }
 
-exports.getTrainingSheets = (req, res) => {
-    const id = req.params.id;
-    console.log(req.body);
-    TrainingSheet.find({owner: id}).then(trainingSheetData => {
+exports.getTrainingSheets = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const trainingSheetData = await TrainingSheet.find({owner: id});
         if (!trainingSheetData) {
             res.status(400).send({ message: "Can't find workout logs" })
         }else{
             res.send(trainingSheetData);
         }
-    });
+    }catch (e) {
+        res.status(500).send({ message: "Hoops! Error ocur" });
+    }
 }
--- app/models/client.model.js
+++ app/models/client.model.js
@@ -1,12 +1,11 @@
 module.exports = mongoose => {
-    const Client = mongoose.model("Client", mongoose.Schema({
+    return mongoose.model("Client", mongoose.Schema({
         userId: String,
-        
+
         name: String,
         age: Number, // years
         weight: Number, // Kg
         height: Number, // meters
         gender: String,
     }, {timestamps: true}));
-    return Client;
 };
